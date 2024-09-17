0682d58dec86a7fe03a80e39e86cd95d18441853
Lucas Sá <lucassnascimento20@gmail.com>
2024-09-17 10:55:15 -0300

    add football example and make refactor

---
+++ kafka/golang-football-example/analytics_consumer.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func AnalyticsConsumer(ctx context.Context, logger *slog.Logger, broker, topic string) error {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{broker},
+		Topic:   topic,
+	})
+	defer r.Close()
+	logger.Info("AnalyticsConsumer started. Waiting for events...")
+
+	var totalScore, teamATotalScore, teamBTotalScore int
+	// Função para atualizar contadores
+	updateScores := func(event Event) {
+		totalScore += event.ScoreTeamA + event.ScoreTeamB // Total de Gols na Partida
+		teamATotalScore += event.ScoreTeamA               // Total de gols Time A
+		teamBTotalScore += event.ScoreTeamB               // Total de gols Time B
+
+		logger.Info("AnalyticsConsumer:",
+			"Match", event.MatchID,
+			"Total Goals", totalScore,
+			"Total Goals: "+event.TeamA, teamATotalScore,
+			"Total Goals: "+event.TeamB, teamBTotalScore)
+	}
+
+	for {
+		// Verifica se o contexto foi cancelado
+		select {
+		case <-ctx.Done():
+			logger.Info("Context canceled, stopping consumer...")
+			return ctx.Err() // Retorna o erro associado ao cancelamento do contexto
+
+		default:
+			// Tenta ler uma nova mensagem
+			m, err := r.ReadMessage(ctx)
+			if err != nil {
+				if err == context.Canceled || err == context.DeadlineExceeded {
+					logger.Info("Context deadline exceeded or canceled, stopping consumer...")
+					return err
+				}
+				logger.Error("could not read message: ", slog.String("error", err.Error()))
+				return err
+			}
+
+			// Transforma o evento de json para struct em go
+			var event Event
+			if err := json.Unmarshal(m.Value, &event); err != nil {
+				logger.Error("unmarshal", slog.String("error", err.Error()))
+				return err
+			}
+
+			// Atualiza os contadores ao receber um novo evento
+			updateScores(event)
+		}
+	}
+}
